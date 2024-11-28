42d1d78906a5434b7710ac02bb11e37dd60575fa
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-11-28 22:22:00 +0900

    🔧 Chore: eslint, prettier 설정
    chore: eslint, prettier 설정

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,6 +1,8 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
+import eslintImport from "eslint-plugin-import"
+import eslintA11y from "eslint-plugin-jsx-a11y"
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
 
@@ -16,9 +18,24 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      import: eslintImport,
+      "jsx-a11y": eslintA11y,
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "no-unused-vars": "warn",
+      "import/order": ["error", { "alphabetize": { "order": "asc" } }],
+      "jsx-a11y/anchor-is-valid": "warn",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-explicit-any": 0,
+      "react/prop-types": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/no-unused-state": "error",
+      "array-callback-return": 0,
+      "react/self-closing-comp": "warn",
+      "@typescript-eslint/ban-ts-comment": "off"
     },
   },
   {
