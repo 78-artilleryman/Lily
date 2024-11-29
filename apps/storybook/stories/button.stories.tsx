5ce9e55eb121ee5f3d5be03a9b246ed75ba174d6
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-11-29 14:58:19 +0900

    🔧 Chore: design-system 환경설정
    chore: design-system 환경설정

---
+++ apps/storybook/stories/button.stories.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@repo/ui/button";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  argTypes: {},
+};
+
+export default meta;
+
+export type Story = StoryObj<typeof Button>;
